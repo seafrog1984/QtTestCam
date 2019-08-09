3c4b2c10147fbef932f095fdd00cc76eecb8a676
seafrog1984 <seafrog1984@hotmail.com>
2019-08-09 19:48:10 +0800

    test bt

--- QtTestCam/QtTestCam/main.cpp
+++ QtTestCam/QtTestCam/main.cpp
@@ -1,11 +1,16 @@
 #include "QtTestCam.h"
 #include <QtWidgets/QApplication>
+#include "QtSetDlg.h"
+
 
 int main(int argc, char *argv[])
 {
 	QApplication::addLibraryPath("./plugins");
 	QApplication a(argc, argv);
-	QtTestCam w;
+	//QtTestCam w;
+	//w.show();
+	QtSetDlg w;
 	w.show();
+
 	return a.exec();
 }
